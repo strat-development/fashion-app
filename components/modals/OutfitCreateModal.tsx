1371851857e45f76e57b6174ee8cce89a1a6a2da
Dawid Feruś <ferero546@gmail.com>
2025-10-28 23:42:05 +0100

    Add OutfitCreateModal re-export and maintain type compatibility

---
+++ components/modals/OutfitCreateModal.tsx
@@ -0,0 +1,6 @@
+// Re-export the refactored OutfitCreateModal
+export { OutfitCreateModal } from './OutfitCreateModal/index';
+
+// Keep backward compatibility by re-exporting types
+export type { OutfitState, PendingImage } from './OutfitCreateModal/types';
+
